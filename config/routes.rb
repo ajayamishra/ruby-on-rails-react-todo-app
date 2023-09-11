48cf3f09d3a59c51b47100499d74dd3ccd132761
Ajaya Mishra <ajaya.mishra.work@gmail.com>
2023-09-11 22:01:34 +0900

    :construction: Update routes to use versioning like /api/v1. Tests are not updated yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :todos
+  # API versioning using /api/v1
+  namespace :api do
+    namespace :v1 do
+      resources :todos
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
